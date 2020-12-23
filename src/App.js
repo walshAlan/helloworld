fbf70046c44e322140fcd6bfaa1c70f9592406c6
Alan Walsh <alanwalsh1994@gmail.com>
2020-12-23 09:06:39 +0000

    hamburger favicon

--- src/App.js
+++ src/App.js
@@ -6,15 +6,6 @@ import {
   Link
 } from "react-router-dom";
 
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function BasicExample() {
   return (
     <Router>
